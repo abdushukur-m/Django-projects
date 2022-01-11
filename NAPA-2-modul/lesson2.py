953db869dcb533917599bfe5facf53e8cfa127e8
abdushukur771 <abdushukur771@mail.ru>
2022-01-11 20:20:39 +0500

    commit

---
+++ NAPA-2-modul/lesson2.py
@@ -0,0 +1,11 @@
+#37
+
+#v1=float(input("V1 ni kiriting (km/soat): "))
+#v2=float(input("V2 ni kiriting (km/soat): "))
+#s0=float(input("Oradagi masofani kiriting (km): "))
+#t=float(input("T vaqtni kiriting (soat): "))
+
+#s=abs(s0-(v1+v2)*t)
+
+#print(f"{t} soatdan keyin ular orasidagi masofa {s} km ga teng")
+
